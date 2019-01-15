62e36e60e4934069673d93f65f69277681c1e109
miohsu <kunieshow@163.com>
2019-01-15 08:14:40 +0800

    no message

--- bbs/tests/test_view_reply_topic.py
+++ bbs/tests/test_view_reply_topic.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.urls import resolve
 from django.contrib.auth import get_user_model
 
 from ..models import Board, Post, Topic
+from ..forms import PostForm
 from ..views import reply_topic
 
 User = get_user_model()
@@ -12,17 +14,14 @@ class ReplyTopicTestCase(TestCase):
     def setUp(self):
         self.board = Board.objects.create(name='Django', description='DjangoTest')
         self.username = 'john'
-        self.password = '123'
-        user = User.objects.create_user(username=self.username, password=self.password, email='john@163.com')
+        self.password = 'root123456'
+        user = User.objects.create_user(username=self.username, email='john@163.com', password=self.password)
         self.topic = Topic.objects.create(subject='TopicTest', board=self.board, starter=user)
         Post.objects.create(message='PostTest', topic=self.topic, created_by=user)
         self.url = reverse('reply_topic', kwargs={'pk': self.board.id, 'topic_pk': self.topic.id})
 
 
 class LoginRequiredReplyTests(ReplyTopicTestCase):
-    def setUp(self):
-        super().setUp()
-
     def test_redirection(self):
         response = self.client.get(self.url)
         login_url = reverse('login')
@@ -30,18 +29,43 @@ class LoginRequiredReplyTests(ReplyTopicTestCase):
 
 
 class ReplyTopicTests(ReplyTopicTestCase):
-    def test_view_reply_response(self):
-        self.client.login(username=self.username, password=self.password)
-        response = self.client.get(self.url)
-        self.assertContains(response, '<input', 1)
-        self.assertContains(response, '<textarea', 1)
+    def setUp(self):
+        super().setUp()
+        self.client.login(username=self.username, passwoord=self.password)
+        self.response = self.client.get(self.url)
+
+    def test_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_view_function(self):
+        view = resolve('/boards/1/topics/1/reply/')
+        self.assertEqual(view.func, reply_topic)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, PostForm)
+
+    def test_form_inputs(self):
+        self.assertContains(self.response, '<input', 1)
+        self.assertContains(self.response, '<textarea', 1)
 
 
 class SuccessfulReplyTopicTests(ReplyTopicTestCase):
-    def test_view_status_code(self):
+    def setUp(self):
+        super().setUp()
         self.client.login(username=self.username, password=self.password)
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 200)
+        self.response = self.client.post(self.url, {'message': 'helloTest'})
+
+    def test_redirection(self):
+        url = reverse('topic_posts', kwargs={'pk': self.board.id, 'topic_pk': self.topic.id})
+        # topic_posts_url = '{url}?page=1#2'.format(url=url)
+        self.assertRedirects(self.response, url)
+
+    def test_reply_created(self):
+        self.assertEqual(Post.objects.count(), 2)
 
 
 class InvalidReplyTopicTests(ReplyTopicTestCase):
